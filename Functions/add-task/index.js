3bcbf5ab92ab62b1539048e045fbcd7da6723080
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2021-03-18 20:19:19 +0100

    added mongo persistence

--- Functions/add-task/index.js
+++ Functions/add-task/index.js
@@ -1,8 +1,24 @@
+
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI ="mongodb+srv://admon:admon@cluster0.w4aqr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+let cachedDb = null;
+async function connectToDatabase() {
+  if (cachedDb) {
+      
+    return cachedDb;
+  }
+  const client = await MongoClient.connect(MONGODB_URI);
+  const db = await client.db("myFirstDatabase");
+  cachedDb = db;
+
+  return db;
+}
+
 exports.handler = async (event) => {
-    
     const task = event;
     let message = JSON.stringify("Error!");
     let code = 400;
+    let test;
     if(typeof(task) != "undefined") {
         
         let taskBody = {
@@ -12,17 +28,17 @@ exports.handler = async (event) => {
             status: task.status,
             dueDate: task.dueDate,
         }
-        
         if (task.description != null && task.name != null && task.email != null && task.status != null && task.dueDate != null){
             message = JSON.stringify("Correct Task Body");
             code = 201;
+            const db = await connectToDatabase();
+            await db.collection("tasks").insertOne(taskBody);
         }
     }
-    
     const res = {
         statusCode: code,
         body: message,
     };
-    return res;
 
+    return res;
 };
--- Functions/get-tasks/index.js
+++ Functions/get-tasks/index.js
@@ -1,8 +1,19 @@
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb+srv://admon:admon@cluster0.w4aqr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+let cachedDb = null;
+async function connectToDatabase() {
+  if (cachedDb) {
+    return cachedDb;
+  }
+  const client = await MongoClient.connect(MONGODB_URI);
+  const db = await client.db("myFirstDatabase");
+  cachedDb = db;
+  return db;
+}
 exports.handler = async (event) => {
 
-    var taskList = [
-        {description: "some desc", name: "Alejandro Bohórquez", email: "sergiobohal@gmail.com", status:"ready", dueDate: "01/01/2021"}
-    ];
+    const db = await connectToDatabase();
+    const taskList = await db.collection("tasks").find({}).toArray();
     
     const responseMessage = { tasks: taskList};
 
